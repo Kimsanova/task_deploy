7eb0d52491ca7187ebbbec92c42930ba51feba5e
Kimsanova <synak2013@gmail.com>
2020-03-05 16:19:05 +0600

    Updates

---
+++ precache-manifest.4b4babf8a5e5670a126817e98c8840fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dedf6ddb0b602dc501beca2e653d5b9",
+    "url": "/task_deploy/index.html"
+  },
+  {
+    "revision": "d17b3e98e37f850e5500",
+    "url": "/task_deploy/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "fc0002b41aee68c71925",
+    "url": "/task_deploy/static/css/main.3bea1e12.chunk.css"
+  },
+  {
+    "revision": "d17b3e98e37f850e5500",
+    "url": "/task_deploy/static/js/2.d94f0eec.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/task_deploy/static/js/2.d94f0eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0002b41aee68c71925",
+    "url": "/task_deploy/static/js/main.bb24767d.chunk.js"
+  },
+  {
+    "revision": "486dbda28dfc7a240f5d",
+    "url": "/task_deploy/static/js/runtime-main.fdb5d5ce.js"
+  },
+  {
+    "revision": "f18dca139aa4abd2d10759050c412715",
+    "url": "/task_deploy/static/media/hero-banner.f18dca13.jpg"
+  },
+  {
+    "revision": "fca2e89eb5e6b35b8b18ca07596e0e3a",
+    "url": "/task_deploy/static/media/logo.fca2e89e.svg"
+  }
+]);
